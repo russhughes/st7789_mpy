3666ac7f05f2e3451ef3ee649763e8521889b1d0
russhughes <russ@owt.com>
2022-11-15 12:37:57 -0800

    tiny_hello.py for tiny screens like t-dongle-s3

---
+++ examples/tiny_hello.py
@@ -0,0 +1,56 @@
+"""
+hello.py
+
+    Writes "Hello!" in random colors at random locations on the display.
+"""
+
+import random
+import utime
+import st7789
+import tft_config
+import vga1_8x8 as font
+
+tft = tft_config.config(0)
+
+def center(text):
+    length = len(text)
+    tft.text(
+        font,
+        text,
+        tft.width() // 2 - length // 2 * font.WIDTH,
+        tft.height() // 2 - font.HEIGHT,
+        st7789.WHITE,
+        st7789.RED)
+
+def main():
+    tft.init()
+
+    tft.fill(st7789.RED)
+    center("Hello!")
+    utime.sleep(2)
+    tft.fill(st7789.BLACK)
+
+    while True:
+        for rotation in range(4):
+            tft.rotation(rotation)
+            tft.fill(st7789.BLACK)
+            col_max = tft.width() - font.WIDTH*6
+            row_max = tft.height() - font.HEIGHT
+
+            for _ in range(128):
+                tft.text(
+                    font,
+                    "Hello!",
+                    random.randint(0, col_max),
+                    random.randint(0, row_max),
+                    st7789.color565(
+                        random.getrandbits(8),
+                        random.getrandbits(8),
+                        random.getrandbits(8)),
+                    st7789.color565(
+                        random.getrandbits(8),
+                        random.getrandbits(8),
+                        random.getrandbits(8)))
+
+
+main()
